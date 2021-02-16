4c56b7c212281f173c729866cf2ea90b346567ad
swimauger <swimauger@gmail.com>
2021-02-15 18:41:38 -0600

    Add source to the current session of the install unix script

--- scripts/install-unix.bash
+++ scripts/install-unix.bash
@@ -4,7 +4,9 @@ if ! command -v node &> /dev/null
 then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
   export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  source ~/.bashrc
   nvm install node
 fi
 
 npm i git+https://github.com/DolphinsGrace/mcsh.git -g
+source ~/.bashrc
